940dcf038582bc5acc3e67531e198c1fa66191af
lilezza <lilreza031@gmailcom>
2025-03-21 19:17:11 +0330

    add admin user list

--- api/adminpanel/urls.py
+++ api/adminpanel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateAdminUser
+from .views import CreateAdminUser , ListAdminUser
 
 urlpatterns = [
     path('create-admin/', CreateAdminUser.as_view(), name='create-admin'),
+    path('list-users/' , ListAdminUser.as_view(), name = 'list-users' ),
 ]
--- api/adminpanel/views.py
+++ api/adminpanel/views.py
@@ -1,10 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-# from rest_framework.permissions import IsAdminUser
 from .serializer import AdminUserSerializer
+from .models import CustomUser
 from rest_framework.permissions import AllowAny  # دسترسی را برای همه باز کردم
+# from rest_framework.permissions import IsAdminUser
 
 class CreateAdminUser(APIView):
     permission_classes = [AllowAny]  # فعلاً همه می‌توانند کاربر ایجاد کنند
@@ -15,6 +15,18 @@ class CreateAdminUser(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ListAdminUser(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self , request):
+        users = CustomUser.objects.all()
+        serializer = AdminUserSerializer(users , many = True)
+        return Response (serializer.data , status=status.HTTP_200_OK)
+
+
+
+
 
 
 # {
