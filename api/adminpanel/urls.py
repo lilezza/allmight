e3af9a894caf139cb9fd003530197de3c9ed3cce
lilezza <lilreza031@gmailcom>
2025-03-21 19:40:55 +0330

    ignore venv folder and change route path

--- api/adminpanel/urls.py
+++ api/adminpanel/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateAdminUser , ListAdminUser
 
 urlpatterns = [
-    path('create-admin/', CreateAdminUser.as_view(), name='create-admin'),
-    path('list-users/' , ListAdminUser.as_view(), name = 'list-users' ),
+    path('create-user/', CreateAdminUser.as_view(), name='create-user'),
+    path('users-list/' , ListAdminUser.as_view(), name = 'users-list' ),
 ]
